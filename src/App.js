70569a7bb3be3e7ba14062b3d038a92a31a57486
wdjty326 <wdjty326@gmail.com>
2023-12-31 22:12:05 +0900

    chore: first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import ContextRouter from './contextRouter';
+import { useState, useMemo, useCallback } from 'react';
+
+import MainPage from './routes/mainPage';
+import JoinPage from './routes/joinPage';
+import LoginPage from './routes/loginPage';
+import ProductHistoryPage from './routes/productHistoryPage';
+import ProductListPage from './routes/productListPage';
+import RewardPointPage from './routes/rewardPointPage';
+
+import './Components.css';
+import RewardPointCompletePage from './routes/rewardPointCompletePage';
 
 function App() {
+  const [prevRouteNames, setPrevRouteNames] = useState([]);
+  const [routeName, setRouteName] = useState(MainPage.name);
+
+  const push = useCallback((name) => {
+    setPrevRouteNames(prev => prev.concat([routeName]));
+    setRouteName(name);
+  }, [routeName]);
+
+  const pop = useCallback(() => {
+    if (prevRouteNames.length === 0) return;
+    const clonePrevRouteNames = prevRouteNames.concat([]);
+    setRouteName(clonePrevRouteNames.pop());
+    setPrevRouteNames(clonePrevRouteNames);
+  }, [prevRouteNames]);
+
+  const pushAndUtilRemoved = useCallback((name) => {
+    setPrevRouteNames([]);
+    setRouteName(name);
+  }, [routeName]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContextRouter.Provider value={{
+      prevRouteNames,
+      push,
+      pushAndUtilRemoved,
+      pop,
+    }}>
+      {routeName === MainPage.name && <MainPage />}
+      {routeName === JoinPage.name && <JoinPage />}
+      {routeName === LoginPage.name && <LoginPage />}
+      {routeName === ProductListPage.name && <ProductListPage />}
+      {routeName === ProductHistoryPage.name && <ProductHistoryPage />}
+      {routeName === RewardPointPage.name && <RewardPointPage />}
+      {routeName === RewardPointCompletePage.name && <RewardPointCompletePage />}
+    </ContextRouter.Provider>
   );
 }
 
